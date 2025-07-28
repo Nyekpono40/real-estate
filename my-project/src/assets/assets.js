cc6ff1908a64a871679a7129de8a134afa112f6f
Nyekpono40 <okoibuakaninyene@gmail.com>
2025-07-28 12:30:15 +0100

    first commit

---
+++ my-project/src/assets/assets.js
@@ -0,0 +1,118 @@
+import project_img1 from './project_img1.jpg'
+import project_img2 from './project_img2.jpeg'
+import project_img3 from './project_img3.jpg'
+import project_img4 from './project_img4.jpg'
+import project_img5 from './project_img5.jpg'
+import project_img6 from './project_img6.jpeg'
+import menu_icon from './menu_icon.png'
+import profile_img1 from './profile_img1.jpg'
+import profile_img2 from './profile_img2.jpg'
+import profile_img3 from './profile_img3.jpg'
+import logo from './logo.png'
+import close_icon from './close_icon.svg'
+import brand1 from './brand1.jpg'
+import brand2 from './brand2.jpg'
+import arrow_left from './arrow_left.png'
+import arrow_right from './arrow_right.png'
+import rating from './rating.png'
+import star from './star.png'
+
+
+
+
+
+
+
+export const assets ={
+    project_img1,
+    project_img2,
+    project_img3,
+    project_img4,
+    project_img5,
+    project_img6,
+    menu_icon,
+    logo,
+    close_icon,
+    brand1,
+    brand2,
+    arrow_left,
+    arrow_right,
+    star
+
+}
+
+
+export const projectData =[
+    {
+        title:'Sky Haven',
+        price:'$2,50,000',
+        location:'paris',
+        image:project_img1
+
+    },
+    {
+        title:'Metro',
+        price:'$2,50,000',
+        location:'New york',
+        image:project_img2
+    },
+    {
+        title:'Vista verde',
+        price:'$2,50,000',
+        location:'Addis Ababa',
+        image:project_img3
+    },
+    {
+        title:'De lux',
+        price:'$2,50,000',
+        location:'California',
+        image:project_img4
+    },
+    {
+        title:'Diamolen',
+        price:'$2,50,000',
+        location:'Los Angeles',
+        image:project_img5
+    },
+    {
+        title:'sapphire',
+        price:'$2,50,000',
+        location:'Lagos',
+        image:project_img6
+    },
+
+]
+
+
+export const testimonialData=[
+    {
+        name: "Donald Jackman",
+        title:'marketing manager',
+        image:profile_img1,
+        alt:'portrait of Donald Jackman',
+        rating:5,
+        rate:rating,
+        text:"from the very first meeting, they understood my vision and helped me find the perfect property. their attention to detail and commitment to client satisfaction  is unmatched."
+
+    },
+    {
+        name: "Zest Victor",
+        title:'Co-founder',
+        image:profile_img2,
+        alt:'portrait of Zest victor',
+        rating:4,
+        rate:rating,
+        text:"from the very first meeting, they understood my vision and helped me find the perfect property. their attention to detail and commitment to client satisfaction  is unmatched."
+
+    },
+    {
+        name: "Ansalem Donald",
+        title:'CEO',
+        image:profile_img3,
+        alt:'portrait of Ansalem Donald',
+        rating:5,
+        rate:rating,
+        text:"from the very first meeting, they understood my vision and helped me find the perfect property. their attention to detail and commitment to client satisfaction  is unmatched."
+
+    }
+]
